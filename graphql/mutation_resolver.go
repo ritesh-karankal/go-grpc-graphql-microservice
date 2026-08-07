7a617b0043432a859883f359e92af96edeb23c01
Ritesh Karankal <ritesh.karankal200@gmail.com>
2026-08-07 19:24:11 +0530

    graphql service code

--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,1 +1,17 @@
 package main
+
+type mutationResolver struct {
+	server *Server
+}
+
+// func (r *mutationResolver) createAccount(ctx context.context, in AccountInput) error {
+
+// }
+
+// func (r *mutationResolver) createProduct(ctx context.context, in ProductInput) error {
+
+// }
+
+// func (r *mutationResolver) createOrder(ctx context.context, in OrderInput) error {
+
+// }
